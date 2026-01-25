ee60b7ce79d95e08985ee33b83b58affa2a079bb
Annie Wong <wong.anni@northeastern.edu>
2026-01-24 19:14:24 -0500

    added default values

--- app/(kambaz)/account/signup/page.tsx
+++ app/(kambaz)/account/signup/page.tsx
@@ -3,8 +3,8 @@ export default function Signup() {
   return ( 
     <div id="wd-signup-screen"> 
       <h3>Sign up</h3> 
-      <input placeholder="username" className="wd-username" /><br/> 
-      <input placeholder="password" type="password" className="wd-password" /><br/> 
+      <input defaultValue="username" placeholder="username" className="wd-username" /><br/> 
+      <input defaultValue="password" placeholder="password" type="password" className="wd-password" /><br/> 
       <input placeholder="verify password" 
              type="password" className="wd-password-verify" /><br/> 
       <Link  href="profile" > Sign up </Link><br /> 
