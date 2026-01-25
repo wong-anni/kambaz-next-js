db61ef3d391cbfccc5d2cb97f6b10072efcd943c
Annie Wong <wong.anni@northeastern.edu>
2026-01-24 19:48:15 -0500

    fixing modules to meet rubric specs

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -11,9 +11,8 @@ export default function Modules() {
 
 
       <ul id="wd-modules"> 
-
         <li className="wd-module"> 
-          <div className="wd-title">Week 1</div> 
+          <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div> 
           <ul className="wd-lessons"> 
             <li className="wd-lesson"> 
               <span className="wd-title">LEARNING OBJECTIVES</span> 
@@ -21,13 +20,68 @@ export default function Modules() {
                 <li className="wd-content-item">Introduction to the course</li> 
                 <li className="wd-content-item">Learn what is Web Development</li> 
               </ul> 
+
+              <span className="wd-title">READING</span>
+              <ul className="wd-content"> 
+                <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li> 
+                <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating User Interfaces with HTML</li> 
+              </ul>
+
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content"> 
+                <li className="wd-content-item">Introduction to Web Development</li> 
+                <li className="wd-content-item">Creating an HTTP server with Node.js</li> 
+                <li className="wd-content-item">Creating a React Application</li> 
+              </ul>
             </li> 
           </ul> 
         </li> 
 
         <li className="wd-module"> 
-          <div className="wd-title">Week 2</div> 
+          <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div> 
+          <ul className="wd-lessons">
+            <li className="wd-lesson"> 
+
+              <span className="wd-title">LEARNING OBJECTIVES</span> 
+              <ul className="wd-content"> 
+                <li className="wd-content-item">How to create user interfaces with HTML</li> 
+                <li className="wd-content-item">Deploy the assignment with Netlify</li> 
+              </ul>
+
+              <span className="wd-title">SLIDES</span> 
+              <ul className="wd-content"> 
+                <li className="wd-content-item">Introduction to HTML and the DOM</li> 
+                <li className="wd-content-item">Formatting Web content with Headings</li> 
+                <li className="wd-content-item">Formatting content with Lists and Tables</li> 
+              </ul>
+
+            </li>
+          </ul>
         </li> 
+
+        <li className="wd-module"> 
+          <div className="wd-title">Week 2, Lecture 1 - Getting Started with the Kambaz Web Application</div> 
+          <ul className="wd-lessons"> 
+            <li className="wd-lesson"> 
+              <span className="wd-title">LEARNING OBJECTIVES</span> 
+              <ul className="wd-content"> 
+                <li className="wd-content-item">Learn how to create user interfaces with HTML</li> 
+                <li className="wd-content-item">Start implementing the Kambaz Web Application</li> 
+                <li className="wd-content-item">Update Vercel deployment</li>
+              </ul> 
+
+              <span className="wd-title">SLIDES</span>
+              <ul className="wd-content"> 
+                <li className="wd-content-item">Implementing the Kambaz Account Screens</li> 
+                <li className="wd-content-item">Implementing the Kambaz Dashboard Screens</li> 
+                <li className="wd-content-item">Implementing the Kambaz Courses Screens</li> 
+                <li className="wd-content-item">Implementing the Kambaz Modules Screens</li> 
+                <li className="wd-content-item">Implementing the Kambaz Assignments Screens</li> 
+
+              </ul>
+            </li> 
+          </ul> 
+        </li>
 
         <li className="wd-module"> 
           <div className="wd-title">Week 3</div> 
