d1e7c27216d80c2b3b68c0143b58834649567607
Annie Wong <wong.anni@northeastern.edu>
2026-02-17 12:02:53 -0500

    intro to javascript done

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,6 +1,21 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+
 export default function Lab3() { 
   return ( 
-    <div> 
+    <div id="wd-lab3"> 
       <h2>Lab 3</h2> 
+        <VariablesAndConstants/>
+        <VariableTypes />
+        <BooleanVariables />
+        <IfElse /> 
+        <TernaryOperator />
+        <ConditionalOutputIfElse />
+        <ConditionalOutputInline /> 
     </div> 
 );} 
