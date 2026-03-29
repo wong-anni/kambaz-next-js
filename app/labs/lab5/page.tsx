e62d0e643c600c99ae7e4ab121a40877cf177d9c
Annie Wong <wong.anni@northeastern.edu>
2026-03-28 20:38:11 -0400

    a5 lab from 5.2.1 to 5.2.4

--- app/labs/lab5/page.tsx
+++ app/labs/lab5/page.tsx
@@ -1,13 +1,26 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+
 export default function Lab5() { 
   return ( 
     <div id="wd-lab5"> 
       <h2>Lab 5</h2> 
       <div className="list-group"> 
-        <a href="http://localhost:4000/lab5/welcome"   // hyperlink navigates to http://localhost:4000/lab5/welcome  
+        <a href={`${HTTP_SERVER}/lab5/welcome`}   // hyperlink navigates to http://localhost:4000/lab5/welcome  
            className="list-group-item"> 
            Welcome 
         </a> 
       </div><hr/> 
+      <EnvironmentVariables /> 
+      <PathParameters />
+      <QueryParameters /> 
+      <WorkingWithObjects />
+      <WorkingWithArrays /> 
     </div> 
 );} 
 
